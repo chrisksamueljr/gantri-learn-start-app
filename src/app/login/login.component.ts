a4a408e71507920c42b648d1d08042aedf7b0b89
Chris Samuel <chrisk.samueljr@gmail.com>
2019-05-09 13:03:41 -0400

    some bug fixes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -11,11 +11,14 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 export class LoginComponent implements OnInit {
 
 
-	 email = new FormControl('', [Validators.required,Validators.email]);
+	 // email = new FormControl('', [Validators.required,Validators.email]);
 
 
-	 getErrorMessage() {
-	 	return this.email.hasError('required') ? 'You must enter a value': 
-	 	this.email.hasError('email') ? 'Not a valid email' : '';
-	 }
+	 // getErrorMessage() {
+	 // 	return this.email.hasError('required') ? 'You must enter a value': 
+	 // 	this.email.hasError('email') ? 'Not a valid email' : '';
+	 // }
+
+
+	 ngOnInit() {}
 }
